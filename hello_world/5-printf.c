ca6c8bf9a18f122b09f797fa4c82c39642f480c5
p-louis17 <p.nsigo@alustudent.com>
2026-07-22 18:18:06 +0200

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+/**
+ *  main - starting point for program execution
+ * description - main will be used as function
+ *  Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
