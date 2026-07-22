b84b5c0fbcc8f494f837779634d125245f3b9203
p-louis17 <p.nsigo@alustudent.com>
2026-07-22 18:12:07 +0200

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
+/*
+ * main - is the starting point for program execution
+ * description - main it the function
+ *success - return 0
+ */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
