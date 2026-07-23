e0ccf9cf65f20973be91bb5ac4eb63374208c288
p-louis17 <p.nsigo@alustudent.com>
2026-07-23 16:05:35 +0200

    101-quote.c

---
+++ hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ *  main - starting point for program execution
+ * description - main will be used as function
+ *  Return: 0 (success)
+ */
+
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
