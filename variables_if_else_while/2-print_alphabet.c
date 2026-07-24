90a0262be9882eafe7560c166b04e6c3422245dd
p-louis17 <p.nsigo@alustudent.com>
2026-07-24 16:48:05 +0200

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ *  main - starting point for program execution
+ * description - main will be used as function
+ *  Return: 0 (success)
+ */
+
+int main(void)
+{	
+	int i = 97;
+	while(i<122)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
