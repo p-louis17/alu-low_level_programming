5839f2c2cac290cf74c874b48070cec342b07791
p-louis17 <p.nsigo@alustudent.com>
2026-07-24 17:25:20 +0200

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,20 +7,19 @@
 
 int main(void)
 {
-        int i = 97;
+	int i = 97;
 	int j = 65;
 
-        while (i < 123)
-        {
-                putchar(i);
-                i++;
-        }
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
 	while (j < 91)
-        {
-                putchar(j);
-                j++;
-        }
-        putchar('\n');
-        return (0);
+	{
+		putchar(j);
+		j++;
+	}
+	putchar('\n');
+	return (0);
 }
-
